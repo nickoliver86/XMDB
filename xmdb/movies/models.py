d4c6c4b53016b43fd09a0a1a66b8043861caa608
Nick <nicholas.oliver@excella.com>
2015-07-08 12:10:22 -0400

    models

--- xmdb/movies/models.py
+++ xmdb/movies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Director(models.Model):
+    name = models.TextField()
+
+class Writer(models.Model):
+    name = models.TextField()
+
+class Actor(models.Model):
+    name = models.TextField
+
+class Movie(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+    director = models.ForeignKey(Director)
+    writer = models.ForeignKey(Writer)
+    actors = models.ManyToManyField(Actor)
+    #STARRATINGS = [i for i in range(1, 6)]
+    #rating = models.IntegerField(max_length=1, choices=STARRATINGS)
+    RATED = (
+        (1, 'G'),
+        (2, 'PG'),
+        (3, 'PG-13'),
+        (4, 'R')
+    )
+    rated = models.IntegerField(choices=RATED)
