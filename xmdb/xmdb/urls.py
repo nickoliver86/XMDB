0b6acaebd60069e344a7c973ffbdd368a16c0592
Kevin <kevin.ellis@excella.com>
2015-07-08 14:31:55 -0400

    No 'movies' required

--- xmdb/xmdb/urls.py
+++ xmdb/xmdb/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^movies/', include('movies.urls', namespace="movies")),
+    url(r'^$/', include('movies.urls', namespace="movies")),
     url(r'^admin/', include(admin.site.urls)),
 ]
