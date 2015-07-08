7542f9087d78ebb58cbd24880f238c940fc7054e
Kevin <kevin.ellis@excella.com>
2015-07-08 11:53:40 -0400

    Removed unecessary stuff. Added landing page. Added index template (basic)

--- xmdb/movies/views.py
+++ xmdb/movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'movies/index.html'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return "hey"
