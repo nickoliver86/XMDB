0327c71e605367dd2238a9ea23737ed3c24fa6cf
Daniel Davis <daniel.davis@excella.com>
2015-07-20 16:45:28 -0400

    Adding in some code review notes

--- xmdb/movies/views.py
+++ xmdb/movies/views.py
@@ -13,14 +13,21 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return "hey"
 
+# FIXME Regarding naming conventions, best to follow pep-8.  
+# methods should be lower case and words separated by underscores
+# Classes can be first letter capitalized (not camel case), no underscores
 
 def IndexTest(request):
     return render(request, 'movies/index.html')
 
 def signin(request):
+    # FIXME What if user is already logged in?
     return render(request, 'movies/signin.html')
 
 def UserSignUp(request):
+    # This is inefficient and dangerous!  Never take input directly from the 
+    # request and store directly in the database!!!  
+    # FIXME Use a ModelForm here
     if request.POST.get('password1') != request.POST.get('password2'):
         return render(request, 'movies/index.html', {'ErrorMessage':'Your passwords must match'})
     username = request.POST.get('username')
@@ -36,6 +43,12 @@ def UserSignUp(request):
 def MovieList(request):
     themovies = Movie.objects.all()
     #The following retrieves movie posters from api for movies added through admin console
+
+    # FIXME We're making requests for every single movie every time we
+    # display the movie list! Why don't we store the meta data in the database
+    # and only grab the stuff we need
+
+    # FIXME Add pagniation, movie list may get long
     for movie in themovies:
         r = requests.get('http://www.omdbapi.com/?t={0}&y=&plot=short&r=json'.format(movie.name.replace(' ', '+')))
         movie.poster = r.json().get('Poster')
@@ -45,6 +58,9 @@ def MovieList(request):
 
 def MovieSummary(request, movie_id):
     m = Movie.objects.get(id=movie_id)
+    # FIXME When we grab this stuff from the database, why do we need to make another
+    # request to the omdbapi?  Why can't we just ensure that when we store the movie
+    # in the database that it's fully parsed so I don't have to call the api again?
     r = requests.get('http://www.omdbapi.com/?t={0}&y=&plot=short&r=json'.format(m.name.replace(' ', '+')))
     try:
         exists_in_library = Movie.objects.get(name=r.json().get('Title')).name
@@ -65,7 +81,8 @@ def Search(request):
         return render(request, 'movies/index.html', {'SearchError':'Movie not found!'})
 
 def AddToLibrary(request, id):
-
+    # FIXME Grab any extra fields we will need in the future and save
+    # so we don't have to make extra requests
     r = requests.get('http://www.omdbapi.com/?i={0}&plot=short&r=json'.format(id))
     j = r.json()
     d = Director.objects.create(name=j.get('Director'))
