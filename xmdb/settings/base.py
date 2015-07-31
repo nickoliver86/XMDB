166f0b8015adb49d0da928c933f41580bb045000
Nick <nicholas.oliver@excella.com>
2015-07-31 12:49:20 -0400

    static files dir

--- xmdb/settings/base.py
+++ xmdb/settings/base.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.abspath(os.path.join(BASE_DIR, 'static', 'movies')),
+    os.path.abspath(os.path.join(BASE_DIR, 'movies', 'static')),
 )
 
 MEDIA_URL = '/media/'
