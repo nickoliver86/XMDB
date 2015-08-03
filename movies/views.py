ed5e9eae6eb0b9e9fb945bfc4eabde522e95f3f4
Nick <nicholas.oliver@excella.com>
2015-08-03 16:15:29 -0400

    movie list images

--- movies/views.py
+++ movies/views.py
@@ -100,7 +100,7 @@ def movie_summary(request, movie_id, search_query=None):
             r = requests.get('http://www.omdbapi.com/?t={0}&y=&plot=short&r=json'.format(search_query.replace(' ', '+')))
         else:
             r = requests.get('http://www.omdbapi.com/?i={0}&plot=short&r=json'.format(imdb))
-        return render(request, 'movies/movie_summary.html', {'movie': new_movie, 'poster': template_poster, 'exists_in_library': False})
+        return render(request, 'movies/movie_summary.html', {'movie': new_movie, 'json': r.json(), 'poster': template_poster, 'exists_in_library': False})
         # return render(request, 'movies/movie_summary.html', {'movie': r.json, 'poster': template_poster, 'exists_in_library': False})
 
     r = requests.get('http://www.omdbapi.com/?t={0}&y=&plot=short&r=json'.format(m.name.replace(' ', '+')))
